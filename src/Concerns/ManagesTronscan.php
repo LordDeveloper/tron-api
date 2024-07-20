e83b7a273d678e8b832d4440be3578badf6234f7
Javad Sadeghi <34248444+LordDeveloper@users.noreply.github.com>
2024-07-20 16:39:48 +0330

    Added search as filter

--- src/Concerns/ManagesTronscan.php
+++ src/Concerns/ManagesTronscan.php
@@ -21,4 +21,20 @@ trait ManagesTronscan
 
         return $this->manager->request('api/transaction', $options);
     }
+
+    /**
+     * Filter transactions from explorer
+     *
+     * @param array $options
+     * @return array
+     * @throws TronException
+     */
+    public function filterTransactions($options = [])
+    {
+        if(empty($options)) {
+            throw new TronException('Parameters must not be empty.');
+        }
+
+        return $this->manager->request('api/multi/search', $options);
+    }
 }
